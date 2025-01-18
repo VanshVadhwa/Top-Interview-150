5a1f4a62216fc31f5b79ca71622423b6e5f22f96
Vansh Vadhwa <vvad0647@gmail.com>
2025-01-19 01:05:42 +0530

    Update 199. Binary Tree Right Side View.cpp

--- Binary Tree BFS/199. Binary Tree Right Side View.cpp
+++ Binary Tree BFS/199. Binary Tree Right Side View.cpp
@@ -10,17 +10,27 @@
  * };
  */
 class Solution {
-private:
-    void rightView(TreeNode* root, int level, vector<int>& ans) {
-        if(!root) return;
-        if(ans.size()==level) ans.push_back(root->val);
-        rightView(root->right,level+1,ans);
-        rightView(root->left,level+1,ans);
-    }
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        rightView(root,0,ans);
+
+        if(!root) {
+            return ans;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            ans.push_back(q.front()->val);
+            int n = q.size();
+            while(n--) {
+                TreeNode* temp = q.front();
+                q.pop();
+                if(temp->right) q.push(temp->right);
+                if(temp->left) q.push(temp->left);
+            }
+        }
         return ans;
     }
 };
