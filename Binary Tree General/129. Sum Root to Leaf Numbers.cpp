89400c5a5087ae065410d40f28f02eaf5ca8cc05
Vansh Vadhwa <vvad0647@gmail.com>
2025-01-30 23:11:28 +0530

    Create 129. Sum Root to Leaf Numbers.cpp

---
+++ Binary Tree General/129. Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        
+    }
+};
+
+// class Solution {
+// private:
+//     void sumRtoL(TreeNode* root, int sum, int& ans) {
+//         if(!root) return;
+
+//         if(!root->left && !root->right) {
+//             ans += sum*10 + root->val;
+//             return;
+//         }
+
+//         sumRtoL(root->left, sum*10+root->val, ans);
+//         sumRtoL(root->right, sum*10+root->val, ans);
+//     }
+// public:
+//     int sumNumbers(TreeNode* root) {
+//         int ans = 0;
+//         sumRtoL(root,0,ans);
+//         return ans;
+//     }
+// };
