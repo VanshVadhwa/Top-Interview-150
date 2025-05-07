ff35f51bf8ba70a92e8bb43d9d77a54cd21405fd
Vansh Vadhwa <vvad0647@gmail.com>
2025-05-07 15:49:17 +0530

    Update 13. Roman to Integer.cpp

--- Array & String/13. Roman to Integer.cpp
+++ Array & String/13. Roman to Integer.cpp
@@ -1,27 +1,26 @@
 class Solution {
 private:
-    int convert(char c) {
-        if(c=='I') return 1;
-        if(c=='V') return 5;
-        if(c=='X') return 10;
-        if(c=='L') return 50;
-        if(c=='C') return 100;
-        if(c=='D') return 500;
-        else 
-        return 1000;
+    int solve(char c) {
+        switch(c) {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+        }
+        return 0;
     }
 public:
     int romanToInt(string s) {
         int n = s.length();
         int ans = 0;
-        
+
         for(int i=0;i<n-1;i++) {
-            int first = convert(s[i]), second = convert(s[i+1]);
-            ans = (first >= second) ? ans + first : ans - first;
+            ans = (solve(s[i]) < solve(s[i+1])) ? ans - solve(s[i]) : ans + solve(s[i]); 
         }
 
-        ans += convert(s[n-1]);
-
-        return ans;
+        return ans+solve(s[n-1]);
     }
 };
